49c546d08e9e62705c8ffa0c4b8af0f6780bb210
HalDonma-prog <HalDonma@icloud.com>
2026-07-31 14:29:57 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,49 +1,88 @@
-function sendMessage() {
+function openChat() {
 
-    const input = document.getElementById("messageInput");
-    const chat = document.getElementById("chatBox");
+    document.getElementById("home").classList.add("hidden");
 
-    if (!input || !chat) {
-        return;
-    }
+    document.getElementById("chatPage")
+    .classList.remove("hidden");
 
-    const text = input.value.trim();
+    document.getElementById("subtitle")
+    .innerText = "Chat";
 
-    if (text.length === 0) {
-        return;
-    }
-
-    const message = document.createElement("div");
-
-    message.className = "message user";
-    message.innerText = text;
-
-    chat.appendChild(message);
-
-    input.value = "";
-
-    setTimeout(function() {
-        chat.scrollTop = chat.scrollHeight;
-    }, 100);
 }
 
 
-// iPhone Tastatur-Unterstützung
-window.onload = function() {
 
-    const input = document.getElementById("messageInput");
+function goHome() {
 
-    if (input) {
+    document.getElementById("chatPage")
+    .classList.add("hidden");
 
-        input.addEventListener("keypress", function(event) {
 
-            if (event.key === "Enter") {
-                event.preventDefault();
-                sendMessage();
-            }
+    document.getElementById("home")
+    .classList.remove("hidden");
 
-        });
+
+    document.getElementById("subtitle")
+    .innerText = "Startseite";
+
+}
+
+
+
+function sendMessage() {
+
+
+    let input = document.getElementById("messageInput");
+
+    let chat = document.getElementById("chatBox");
+
+
+    let text = input.value.trim();
+
+
+    if(text === "") {
+
+        return;
 
     }
+
+
+    let message = document.createElement("div");
+
+
+    message.className = "message user";
+
+
+    message.innerText = text;
+
+
+    chat.appendChild(message);
+
+
+    input.value = "";
+
+
+    chat.scrollTop = chat.scrollHeight;
+
+}
+
+
+
+window.onload = function(){
+
+    let input = document.getElementById("messageInput");
+
+
+    input.addEventListener("keypress", function(event){
+
+        if(event.key === "Enter"){
+
+            event.preventDefault();
+
+            sendMessage();
+
+        }
+
+    });
 
 };
