b09c0a3e3621a5424a7fd42b13ccf548a9fb887d
HalDonma-prog <HalDonma@icloud.com>
2026-07-31 09:30:19 +0200

    chat.js aktualisieren

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
+alert("HalDo Chat JS geladen");
 alert("chat.js wird geladen");
-
 document.addEventListener("DOMContentLoaded", () => {
     const chatBox = document.getElementById("chatBox");
     const userInput = document.getElementById("userInput");
