639e9a95c5a9173c5df615259563c8fd6b730fc4
HalDonma-prog <HalDonma@icloud.com>
2026-07-31 14:11:17 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function senden() {
-  let text = document.getElementById("textInput").value;
-  document.getElementById("ausgabe").innerHTML = text;
+function sendMessage() {
+
+    let input = document.getElementById("messageInput");
+    let chat = document.getElementById("chatBox");
+
+    let text = input.value;
+
+    if (text === "") {
+        return;
+    }
+
+    let message = document.createElement("p");
+    message.textContent = "Du: " + text;
+
+    chat.appendChild(message);
+
+    input.value = "";
+
 }
