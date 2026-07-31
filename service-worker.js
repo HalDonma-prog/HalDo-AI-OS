e80ba1c96e060f54bc4bcd451e5e0af52869a0ca
HalDonma-prog <HalDonma@icloud.com>
2026-07-31 15:21:03 +0200

    service-worker.js aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,46 @@
+const cacheName = "apps-web-3-v1";
 
+
+const files = [
+
+"index.html",
+"style.css",
+"script.js",
+"manifest.json"
+
+];
+
+
+self.addEventListener("install", function(event){
+
+event.waitUntil(
+
+caches.open(cacheName)
+
+.then(function(cache){
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+
+self.addEventListener("fetch", function(event){
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(function(response){
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
