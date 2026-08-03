32bf05db620e22ec53ce2abe401947d10be54fef
HalDonma-prog <HalDonma@icloud.com>
2026-08-03 09:10:40 +0200

    service-worker.js erstellen

---
+++ HalDo ai /worker.js/service-worker.js
@@ -0,0 +1,51 @@
+const cacheName="apps-web-3.0-v2";
+
+
+const files=[
+
+"index.html",
+"style.css",
+"script.js",
+"manifest.json",
+"icon.png"
+
+];
+
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(cacheName)
+
+.then(cache=>{
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
