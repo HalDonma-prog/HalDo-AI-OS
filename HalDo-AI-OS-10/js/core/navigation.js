99a68e34d097b86af14cfa688a3a8cbfb82814c6
HalDonma-prog <HalDonma@icloud.com>
2026-08-04 21:47:06 +0200

    navigation.js erstellen

---
+++ HalDo-AI-OS-10/js/core/navigation.js
@@ -0,0 +1,14 @@
+// ==========================================
+// HalDo AI OS Professional 10.0
+// Navigation Core
+// ==========================================
+
+
+function openSystem() {
+
+    console.log("🚀 Öffne HalDo AI OS Dashboard");
+
+
+    window.location.href = "dashboard.html";
+
+}
