c0283ff7467aaa23b5d67f9d4d03f7b4e8cfe022
HalDonma-prog <HalDonma@icloud.com>
2026-08-06 01:55:55 +0200

    settings.js erstellen

---
+++ HalDo-AI-OS-10/apps/settings/settings.js
@@ -0,0 +1,28 @@
+// =================================
+// HalDo Settings System
+// =================================
+
+
+console.log(
+"HalDo Settings geladen"
+);
+
+
+
+function changeLanguage(){
+
+alert(
+"Sprachverwaltung öffnen"
+);
+
+}
+
+
+
+function checkUpdates(){
+
+alert(
+"Suche nach Updates..."
+);
+
+}
