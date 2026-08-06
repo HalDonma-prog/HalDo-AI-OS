28b6f7240adf3f78a83655155b8d22f9ee0fd413
HalDonma-prog <HalDonma@icloud.com>
2026-08-06 22:58:12 +0200

    system.js erstellen

---
+++ HalDo-AI-OS-10/js/system.js
@@ -0,0 +1,43 @@
+// =================================
+// HalDo AI OS 18
+// System Manager
+// =================================
+
+
+const HalDoSystem = {
+
+
+modules:[],
+
+
+initialize(){
+
+
+console.log(
+"🔵 Systemprüfung läuft..."
+);
+
+
+this.check();
+
+
+},
+
+
+
+check(){
+
+
+console.log(
+"🟢 Grundsystem bereit"
+);
+
+
+}
+
+
+};
+
+
+
+HalDoSystem.initialize();
