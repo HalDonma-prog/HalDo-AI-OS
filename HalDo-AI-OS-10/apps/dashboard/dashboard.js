75f401bb3aa2cf966e5ce52e8351cf65b2cbc02d
HalDonma-prog <HalDonma@icloud.com>
2026-08-06 12:50:44 +0200

    dashboard.js aktualisieren

--- HalDo-AI-OS-10/apps/dashboard/dashboard.js
+++ HalDo-AI-OS-10/apps/dashboard/dashboard.js
@@ -1,90 +1,10 @@
-// ==========================================
-// HalDo Dashboard
-// ==========================================
+// =================================
+// HalDo AI OS Dashboard
+// =================================
 
 
-const apps = [
+console.log(
 
+"Dashboard gestartet"
 
-{
-name:"HalDo AI Chat",
-icon:"🤖",
-link:"../chat/index.html"
-},
-
-
-{
-name:"Language Center",
-icon:"🌍",
-link:"#"
-},
-
-
-{
-name:"Settings",
-icon:"⚙️",
-link:"../settings/index.html"
-},
-
-
-{
-name:"Health Center",
-icon:"❤️",
-link:"#"
-}
-
-
-];
-
-
-
-const container =
-document.getElementById("apps");
-
-
-
-apps.forEach(app=>{
-
-
-let card =
-document.createElement("div");
-
-
-card.className="card";
-
-
-
-card.innerHTML=`
-
-<div class="icon">
-${app.icon}
-</div>
-
-<h3>
-${app.name}
-</h3>
-
-
-<button onclick="openApp('${app.link}')">
-
-Öffnen
-
-</button>
-
-`;
-
-
-
-container.appendChild(card);
-
-
-});
-
-
-
-
-function openApp(link){
-
-window.location.href=link;
-
-}
+);
