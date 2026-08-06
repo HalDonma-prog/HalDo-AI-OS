a7954c47fceb00d1eec8e03754554f0296dfee89
HalDonma-prog <HalDonma@icloud.com>
2026-08-06 22:56:42 +0200

    kernel.js erstellen

---
+++ HalDo-AI-OS-10/js/kernel.js
@@ -0,0 +1,29 @@
+// =================================
+// HalDo AI OS 18
+// Kernel
+// =================================
+
+
+const HalDoKernel = {
+
+
+version:"18.0",
+
+
+start(){
+
+console.log(
+"🔵 Kernel gestartet"
+);
+
+
+return true;
+
+}
+
+
+};
+
+
+
+HalDoKernel.start();
